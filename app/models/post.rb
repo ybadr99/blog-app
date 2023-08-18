9c56dad15cc1a56d868f54e2042b70ef03a77ca6
ybadr99 <yousefbadr909@gmail.com>
2023-08-18 05:54:37 +0300

    processing data in models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,12 @@
 class Post < ApplicationRecord
+  belongs_to :author, class_name: 'User'
+  has_many :comments
+
+  def update_posts_counter
+    author.update(posts_counter: author.posts.count)
+  end
+
+  def recent_comments(limit = 5)
+    comments.order(created_at: :desc).limit(limit)
+  end
 end
