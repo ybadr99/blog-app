3710de2cd829ef213dc705058c6212c03ae64d69
ybadr99 <yousefbadr909@gmail.com>
2023-08-19 08:25:21 +0300

    validate user and post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,6 +5,10 @@ class Post < ApplicationRecord
 
   after_save :update_post_counter
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :likes_counter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
   def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
   end
