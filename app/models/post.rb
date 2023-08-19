12ec9d43d2b0a42adf5155e64ba49dbe602ebfb5
ybadr99 <yousefbadr909@gmail.com>
2023-08-19 07:41:24 +0300

    update assocations logic

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,17 @@
 class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments
+  has_many :likes
 
-  def update_posts_counter
-    author.update(posts_counter: author.posts.count)
-  end
-
+  after_save :update_post_counter
+  
   def recent_comments(limit = 5)
     comments.order(created_at: :desc).limit(limit)
+  end
+
+  private
+
+  def update_post_counter
+    author.increment!(:posts_counter)
   end
 end
