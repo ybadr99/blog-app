c704ea64f2cb021021765ceadd7ee2453d00cae1
ybadr99 <yousefbadr909@gmail.com>
2023-08-26 13:08:26 +0300

    implement views

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,19 @@
 class PostsController < ApplicationController
-  def index; end
+  before_action :find_user
 
-  def show; end
+  def index
+    # @posts = @user.posts
+
+    @posts = Post.paginate(page: params[:page], per_page: 5)
+  end
+
+  def show
+    @post = @user.posts.find(params[:id])
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end
 end
